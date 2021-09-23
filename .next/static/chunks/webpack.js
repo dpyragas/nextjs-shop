206bc44ef9e017add549ebb97d9162f105b63092
dpyragas <daumantas.pyragas@gmail.com>
2021-09-23 14:30:18 +0100

    Changing URL

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7f8f0cfd58c7f0161a61"; }
+/******/ 		__webpack_require__.h = function() { return "61aac31627360b8fd512"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
