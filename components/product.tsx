0d8e3a0f6c7affe6eac0d5d35ea39694decc5de6
dpyragas <daumantas.pyragas@gmail.com>
2021-09-23 14:02:07 +0100

    Forgot type

--- components/product.tsx
+++ components/product.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { IProduct } from '../models/product';
 
-const Product = ({ product }) => {
+const Product = ({ product }: any) => {
   return (
     <div>
       <a>
